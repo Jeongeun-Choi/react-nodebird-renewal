f5f2ca3eae638255cd746fdbf70301d103c6408b
Jeongeun-Choi <cje3080@gmail.com>
2020-06-09 16:38:13 +0900

    custom hook 만들어보기

--- front/components/LoginForm.js
+++ front/components/LoginForm.js
@@ -2,6 +2,8 @@ import React, {useState, useCallback} from 'react';
 import {Form, Input, Button} from 'antd';
 import Link from 'next/link';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import useInput from '../hooks/useInput';
 
 const ButtonWrapper = styled.div`
     margin-top: 10px;
@@ -12,16 +14,9 @@ const FormWrapper = styled(Form)`
 `;
 
 const LoginForm = ({setIsLoggedIn}) => {
-    const [id, setId] = useState('');
-    const [password, setPassword] = useState('');
+    const [id, onChangeId] = useInput('');
+    const [password, onChangePassword] = useInput('');
 
-    const onChangeId = useCallback((e) => {
-        setId(e.target.value);
-    }, []);
-
-    const onChangePassword = useCallback((e) => {
-        setPassword(e.target.value);
-    }, []);
 
     const onSubmitForm = useCallback(() => {
         console.log(id, password);
@@ -60,4 +55,7 @@ const LoginForm = ({setIsLoggedIn}) => {
     );
 };
 
+LoginForm.propTypes = { 
+    setIsLoggedIn : PropTypes.func.isRequired
+}
 export default LoginForm;
